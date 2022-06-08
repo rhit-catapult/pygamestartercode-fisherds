f5fc5e6b01ef8a5010489cb2587d95994d4cfcf9
David Fisher <fisherds@rose-hulman.edu>
2022-06-08 16:53:57 -0400

    loops and sequences

--- 00-IntroToPython/python_syntax_guide.py
+++ 00-IntroToPython/python_syntax_guide.py
@@ -25,11 +25,54 @@ def strings():
     print(str5)
 
 
+def loops():
+    print("----- Loops -------")
+    for k in range(5):
+        print(k)
+    total = 0
+    for k in range(101):
+        total = total + k
+    print(f"The numbers 1 to 100 add up to {total}.")
+
+    total = 1
+    for k in range(100):
+        total = total * (k + 1)
+    print(f"The numbers 1 to 100 multiply to {total}.")
+
+    x = 0
+    while True:
+        x += 1
+        if x > 5:
+            break
+    print(f"x is equal to {x}")
+
+
+def sequences():
+    print("----- Sequences -------")
+    my_list = [4, 5, 6, 7]
+    print(my_list)
+    my_list[2] = 99
+    my_list.append(101)
+    print(my_list)
+    print(f"The length of the list is {len(my_list)}.")
+    for k in range(len(my_list)):
+        my_list[k] = my_list[k] + 10
+    print(my_list)
+
+    total = 0
+    for value in my_list:
+        total += value
+    print(f"The total value of all items is {total}")
+
+
+
 def main():
     # This line is a comment.  The next line is Python code.
     print("Python Syntax Guide!")
-    variables()
-    strings()
+    # variables()
+    # strings()
+    # loops()
+    sequences()
 
 
 main()
