5545db107ba2c782d5d260c4526a776f87d0d561
David Fisher <fisherds@rose-hulman.edu>
2022-06-11 11:31:51 -0400

    Tic_Tac_Toe done!

--- 06-TicTacToe/TicTacToe.py
+++ 06-TicTacToe/TicTacToe.py
@@ -29,34 +29,62 @@ def get_xy_position(row, col):
 
 class Game:
     def __init__(self):
-        # TODO 5: Create an empty board, called board
+        # Done 5: Create an empty board, called board
         #         A list that contains 3 lists, each of those lists has 3 "." values.
         #     - Create a game_state_string set to X's turn
         #     - Create a turn_counter variable set to 0
         #     - Create a game_is_over variable set to False
-        pass
+        self.board = [[".", ".", "."], [".", ".", "."], [".", ".", "."]]
+        self.game_state_string = "X's turn"
+        self.turn_counter = 0
+        self.game_is_over = False
 
     def __repr__(self):
         """ Returns a string that represents the game. """
-        # TODO 7: Use a "".format() command to create a string to shows the board, turn_counter, and game_state_string
+        # Done 7: Use a "".format() command to create a string to shows the board, turn_counter, and game_state_string
+        return f"State: {self.game_state_string}\nBoard: {self.board}\nTurn: {self.turn_counter}\n"
 
     def take_turn(self, row, col):
         """Handle the current turn of the player and update board array"""
-        # TODO 8: Check if game_is_over and return from this method (doing nothing) if True
-        # TODO 9: Check if the value for row and col are valid.  Return (doing nothing) if invalid.
-        # TODO 10: Check if the mark at the requested row col is ".".  Return (doing nothing) if it is not "."
+        # Done 8: Check if game_is_over and return from this method (doing nothing) if True
+        # Done 9: Check if the value for row and col are valid.  Return (doing nothing) if invalid.
+        # Done 10: Check if the mark at the requested row col is ".".  Return (doing nothing) if it is not "."
+        if self.game_is_over:
+            print("The game is over.  You can't take turns")
+            return
+        if row < 0 or row > 2 or col < 0 or col > 2:
+            print("Invalid row or col value")
+            return
+        if self.board[row][col] != ".":
+            print("That spot is not empty")
+            return
 
-        # TODO 11: Determine if it is X's turn or O's turn (even turn_counter means X's turn, odd for O's turn)
+        # Done 11: Determine if it is X's turn or O's turn (even turn_counter means X's turn, odd for O's turn)
         #     - Modify the board by setting the current row col to an "X" or an "O" as appropriate
         #     - Update the game_state_string as appropriate "O's Turn" or "X's Turn"
+        if self.turn_counter % 2 == 0:
+            # Even --> Must be X turn
+            self.board[row][col] = "X"
+            self.game_state_string = "O's Turn"
+        else:
+            # Odd --> Must be O turn
+            self.board[row][col] = "O"
+            self.game_state_string = "X's Turn"
 
-        # TODO 12: Increment the turn_counter
+        # Done 12: Increment the turn_counter
+        self.turn_counter += 1
+
+
 
         self.check_for_game_over()
 
     def check_for_game_over(self):
-        # TODO 18: If the turn_counter is 9 then the game is over
+        # Done 18: If the turn_counter is 9 then the game is over
         #      If >=9 update the game_is_over value and set the game_state_string to "Tie Game"
+        if self.turn_counter >= 9:
+            self.game_is_over = True
+            self.game_state_string = "Tie Game"
+
         lines = []
         lines.append(self.board[0][0] + self.board[0][1] + self.board[0][2])
         lines.append(self.board[1][0] + self.board[1][1] + self.board[1][2])
@@ -67,8 +95,15 @@ class Game:
         lines.append(self.board[0][0] + self.board[1][1] + self.board[2][2])
         lines.append(self.board[0][2] + self.board[1][1] + self.board[2][0])
 
-        # TODO 19: Use the lines list to determine if there is a winner.
+        # Done 19: Use the lines list to determine if there is a winner.
         #    If there is a winner, update the game_state_string, play a sound, and set game_is_over to True.
+        for line in lines:
+            if line == "XXX" or line == "OOO":
+                self.game_is_over = True
+                pygame.mixer.music.play()
+                self.game_state_string = "X Wins!"
+                if line == "OOO":
+                    self.game_state_string = "O Wins!"
 
 
 # --------------------------- View Controller ---------------------------
@@ -77,32 +112,56 @@ class ViewController:
 
     def __init__(self, screen):
         """ Creates the view controller (the Tic-Tac-Toe game you see) """
-        # TODO 4: Initialize the ViewController, as follows:
+        # Done 4: Initialize the ViewController, as follows:
         #     - Store the screen.
         #     - Create the game model object.
         #     - Create images for the board, X, and O images filenames.
         #  Use instance variables:   screen game board_image x_image o_image
-        pass
+        self.screen = screen
+        self.game = Game()
+        self.image_board = pygame.image.load("board.png")
+        self.image_x = pygame.image.load("x_mark.png")
+        self.image_o = pygame.image.load("o_mark.png")
 
     def check_event(self, event):
         """ Takes actions as necessary based on the current event. """
-        # TODO 16: If the event is pygame.MOUSEBUTTONUP
+        # Done 16: If the event is pygame.MOUSEBUTTONUP
         #     Get the mouse click position as x and y variables
         #     Convert the x and y variables into row and col using get_row_col
         #     Inform the model object about this event
-        # TODO 17: If the event is pygame.KEYDOWN
+        # Done 17: If the event is pygame.KEYDOWN
         #     Get the pressed_keys
         #     If the key is pygame.K_SPACE, then reset the game.
-        pass
+        if event.type == pygame.MOUSEBUTTONUP:
+            # click_position = pygame.mouse.get_pos()
+            click_position = event.pos  # Another mouse option
+            row, col = get_row_col(click_position[0], click_position[1])
+            print(f"You clicked row {row} col {col}")
+            self.game.take_turn(row, col)
+        if event.type == pygame.KEYDOWN:
+            pressed_keys = pygame.key.get_pressed()
+            if pressed_keys[pygame.K_SPACE]:
+                self.game = Game()
 
     def draw(self):
         """ Draw the board based on the marked store in the board configuration array """
-        # TODO 13: Blit the board_image onto the screen at the x y position of row=0 col=0
-        # TODO 14: Use a nested loop (via range) to go over all marks of the game.board
+        # Done 13: Blit the board_image onto the screen at the x y position of row=0 col=0
+        # Done 14: Use a nested loop (via range) to go over all marks of the game.board
         #    If the mark is "X", blit an X image at the x y position of row col
         #    If the mark is "O", blit an O image at the x y position of row col
-        # TODO 15: Update the display caption to be the game.game_state_string
-        pass
+        # Done 15: Update the display caption to be the game.game_state_string
+        self.screen.blit(self.image_board, get_xy_position(0,0))
+        for row in range(3):
+            for col in range(3):
+                xy_location = get_xy_position(row, col)
+                mark = self.game.board[row][col]
+                if mark == "X":
+                    self.screen.blit(self.image_x, xy_location)
+                if mark == "O":
+                    self.screen.blit(self.image_o, xy_location)
+
+        pygame.display.set_caption(self.game.game_state_string)
+
 
 # --------------------------- Controller ---------------------------
 
@@ -111,18 +170,36 @@ def main():
     pygame.init()
     pygame.mixer.music.load("win.mp3")
     screen = pygame.display.set_mode((380, 400))
-    # TODO 1: Create an instance of the ViewController class called view_controller
+    # Done 1: Create an instance of the ViewController class called view_controller
+    view_controller = ViewController(screen)
 
-    # TODO 6: Write test code as needed to develop your model object.
+    # In progress 6: Write test code as needed to develop your model object.
+    # print(view_controller.game)
+    # view_controller.game.take_turn(1, 1)
+    # print(view_controller.game)
+    #
+    # view_controller.game.take_turn(0, 0)
+    # print(view_controller.game)
+    #
+    # view_controller.game.take_turn(0, 1)
+    # print(view_controller.game)
+    #
+    # view_controller.game.take_turn(0, 1)
+    # print(view_controller.game)
+    #
+    # view_controller.game.take_turn(0, 2)
+    # print(view_controller.game)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            # TODO 2: Pass the event to the view_controller
+            # Done 2: Pass the event to the view_controller
+            view_controller.check_event(event)
 
         screen.fill(pygame.Color("white"))
-        # TODO 3: Draw the view_controller
+        # Done 3: Draw the view_controller
+        view_controller.draw()
         pygame.display.update()
 
 
