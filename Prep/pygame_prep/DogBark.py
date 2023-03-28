b2cfcc570b9628a0bc07e887d349bdd55944bf7f
David Fisher <fisherds@rose-hulman.edu>
2023-03-28 18:26:27 -0400

    prep for dog bark

---
+++ Prep/pygame_prep/DogBark.py
@@ -0,0 +1,73 @@
+import pygame
+import sys
+
+
+def main():
+    # pre-define RGB colors for Pygame
+    BLACK = pygame.Color("Black")
+    WHITE = pygame.Color("White")
+    IMAGE_SIZE = 470
+    TEXT_HEIGHT = 30
+
+    # initialize the pygame module
+    pygame.init()
+
+    # prepare the window (screen)
+    screen = pygame.display.set_mode((IMAGE_SIZE, IMAGE_SIZE + TEXT_HEIGHT))
+    pygame.display.set_caption("Text, Sound, and an Image")
+
+    # print(pygame.font.get_fonts())
+
+    # Prepare the image
+    # 1: Create an image with the 2dogs.JPG image
+    dog_image = pygame.image.load("2dogs.JPG")
+    #  3: Scale the image to be the size (IMAGE_SIZE, IMAGE_SIZE)
+    dog_image = pygame.transform.scale(dog_image, (IMAGE_SIZE, IMAGE_SIZE))
+
+    # Prepare the text caption(s)
+    # 4: Create a font object with a size 28 font.
+    # font1 = pygame.font.SysFont("comicsansms", 28)
+    font1 = pygame.font.SysFont("verdana", 28)
+    #  5: Render the text "Two Dogs" using the font object (it's like MAKING an image).
+    caption1 = font1.render("Two Dogs", True, BLACK)
+
+    font2 = pygame.font.SysFont("impact", 50)
+    caption2 = font2.render("This human is mine!", True, WHITE)
+
+    # Prepare the music
+    # TODO 8: Create a Sound object from the "bark.wav" file.
+    bark_sound = pygame.mixer.Sound("bark.wav")  # Sound effects
+    pygame.mixer.music.load("bark.mp3")  # Load a song for background music
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # bark_sound.play()
+                pygame.mixer.music.play(-1)
+            if event.type == pygame.MOUSEBUTTONUP:
+                pygame.mixer.music.stop()
+            # TODO 9: Play the music (bark) if there's a mouse click.
+
+        # Clear the screen and set the screen background
+        screen.fill(WHITE)
+
+        # Draw the image onto the screen
+        #  2: Draw (blit) the image onto the screen at position (0, 0)
+        screen.blit(dog_image, (0, 0))
+
+        # Draw the text onto the screen
+        #  6: Draw (blit) the text image onto the screen in the middle bottom.
+        # Hint: Commands like these might be useful..
+        #          screen.get_width(), caption1.get_width(), image1.get_height()
+        screen.blit(caption1, (screen.get_width() / 2 - caption1.get_width() / 2, screen.get_height() - caption1.get_height()))
+
+        #  7: On your own, create a new bigger font and in white text place a 'funny' message on top of the image.
+        screen.blit(caption2, (screen.get_width() / 2 - (caption2.get_width() / 2), 400))
+
+        # Update the screen
+        pygame.display.update()
+
+
+main()
--- Prep/pygame_prep/MovingSmile.py
+++ Prep/pygame_prep/MovingSmile.py
@@ -6,15 +6,37 @@ def main():
     pygame.init()
     pygame.display.set_caption("Moving Smile")
     screen = pygame.display.set_mode((640, 480))
+    is_mouth_visible = True
+    is_nose_visible = False
+    eye_x = 0
+    eye_y = 0
+    clock = pygame.time.Clock()
 
     while True:
-        # TODO 4: Set the clock speed to 60 fps
+        # 4: Set the clock speed to 60 fps
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            # TODO 3: Make the eye pupils move with Up, Down, Left, and Right keys
+            # 3: Make the eye pupils move with Up, Down, Left, and Right keys
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                is_mouth_visible = not is_mouth_visible
+            if event.type == pygame.KEYDOWN:
+                keys_pressed = pygame.key.get_pressed()
+                if keys_pressed[pygame.K_SPACE]:
+                    is_nose_visible = not is_nose_visible
 
         screen.fill((255, 255, 255))  # white
+
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_LEFT]:
+            eye_x -= 2
+        if keys_pressed[pygame.K_RIGHT]:
+            eye_x += 2
+        if keys_pressed[pygame.K_UP]:
+            eye_y -= 2
+        if keys_pressed[pygame.K_DOWN]:
+            eye_y += 2
 
         # API --> pygame.draw.circle(screen, color, (x, y), radius, thickness)
 
@@ -23,19 +45,23 @@ def main():
 
         pygame.draw.circle(screen, (225, 225, 225), (240, 160), 25)  # white eye
         pygame.draw.circle(screen, (0, 0, 0), (240, 160), 25, 3)  # black outline
-        pygame.draw.circle(screen, (0, 0, 0), (242, 162), 7)  # black pupil
+        pygame.draw.circle(screen, (0, 0, 0), (242 + eye_x, 162 + eye_y), 7)  # black pupil
 
         pygame.draw.circle(screen, (225, 225, 225), (400, 160), 25)  # white eye
         pygame.draw.circle(screen, (0, 0, 0), (400, 160), 25, 3)  # black outline
-        pygame.draw.circle(screen, (0, 0, 0), (398, 162), 7)  # black pupil
+        pygame.draw.circle(screen, (0, 0, 0), (398 + eye_x, 162 + eye_y), 7)  # black pupil
 
-        # TODO 1: Draw a nose
+        # Done 1: Draw a nose
         # Suggestion: color (80,0,0) location (320,245), radius 10
         # API --> pygame.draw.circle(screen, (r,g,b), (x, y), radius, thickness)
+        if is_nose_visible:
+            pygame.draw.circle(screen, (80,0,0), (320,245), 10)
 
-        # TODO 2: Draw a mouth
+        # Done 2: Draw a mouth
         # Suggestion: color (0,0,0), x 230, y 320, width 180, height 30
         # API --> pygame.draw.rect(screen, (r,g,b), (x, y, width, height), thickness)
+        if is_mouth_visible:
+            pygame.draw.rect(screen, (0,0,0), (230, 320, 180, 30))
 
         pygame.display.update()
 
