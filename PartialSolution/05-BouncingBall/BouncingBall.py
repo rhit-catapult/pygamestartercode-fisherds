094c3a44cd3d848c3e5eb6a7d47cca1475232cd5
David Fisher <fisherds@rose-hulman.edu>
2023-04-19 12:19:49 -0400

    prep for TTT

--- PartialSolution/05-BouncingBall/BouncingBall.py
+++ PartialSolution/05-BouncingBall/BouncingBall.py
@@ -3,21 +3,54 @@ import sys
 import random
 
 
-# You will implement this module ENTIRELY ON YOUR OWN!
+class Ball:
 
-# TODO: Create a Ball class.
-# TODO: Possible member variables: screen, color, x, y, radius, speed_x, speed_y
-# TODO: Methods: __init__, draw, move
+    def __init__(self, screen, x, y, color, radius):
+        self.screen = screen
+        self.color = color
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.speed_x = random.randint(-5, 5)  # Note, it might pick 0, oh well
+        self.speed_y = random.randint(-5, 5)  # Note, it might pick 0, oh well
+
+    def draw(self):
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
+
+    def move(self):
+        self.x = self.x + self.speed_x
+        self.y = self.y + self.speed_y
+        if self.x + self.radius >= self.screen.get_width():
+            self.speed_x = -self.speed_x
+        if self.y + self.radius >= self.screen.get_height():
+            self.speed_y = -self.speed_y
+        if self.x - self.radius <= 0:
+            self.speed_x = -self.speed_x
+        if self.y - self.radius <= 0:
+            self.speed_y = -self.speed_y
 
 
 def main():
+    demo_with_1_ball_only = False
     pygame.init()
-    screen = pygame.display.set_mode((300, 300))
+    if demo_with_1_ball_only:
+        screen = pygame.display.set_mode((300, 300))
+    else:
+        screen = pygame.display.set_mode((1000, 800))
     pygame.display.set_caption('Bouncing Ball')
+    screen.fill(pygame.Color('gray'))
+
     clock = pygame.time.Clock()
 
-    # TODO: Create an instance of the Ball class called ball1
-
+    if demo_with_1_ball_only:
+        ball = Ball(screen, 70, 70, pygame.Color("green"), 10)
+    else:
+        balls = []
+        for k in range(100):
+            new_ball = Ball(screen, random.randint(10, 290), random.randint(10, 290),
+                            (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
+                            random.randint(8, 12))
+            balls.append(new_ball)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -26,19 +59,15 @@ def main():
         clock.tick(60)
         screen.fill(pygame.Color('gray'))
 
-        # TODO: Move the ball
-        # TODO: Draw the ball
+        if demo_with_1_ball_only:
+            ball.move()
+            ball.draw()
+        else:
+            for ball in balls:
+                ball.move()
+                ball.draw()
 
         pygame.display.update()
 
 
 main()
-
-
-# Optional challenges (if you finish and want do play a bit more):
-#   After you get 1 ball working make a few balls (ball2, ball3, etc) that start in different places.
-#   Make each ball a different color
-#   Make the screen 1000 x 800 to allow your balls more space (what needs to change?)
-#   Make the speed of each ball randomly chosen (1 to 5)
-#   After you get that working try making a list of balls to have 100 balls (use a loop)!
-#   Use random colors for each ball
